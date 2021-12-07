fd52069b075caf974469e748fc9e92d042266a8b
Arne Caluwe <arne.caluwe@outlook.com>
2021-12-07 09:11:14 +0100

    Add Profile displays (small')

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <NamedprofileIcon />
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import "../scss/main.scss";
-import HelloWorld from "./components/HelloWorld.vue";
+import NamedprofileIcon from "./components/profiles/NamedprofileIcon.vue";
 </script>
 
 
---
+++ src/components/profiles/ProfileIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="profile-border">
+    <div class="image-template"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfileIcon",
+  props: {
+    color: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.profile-border {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: red;
+}
+.image-template {
+  text-align: center;
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+  background-color: white;
+}
+</style>
